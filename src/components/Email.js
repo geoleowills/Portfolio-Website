665a0e9b87ba4e88676284ee7a77e256e476abf3
George Willens <georgewillens@Georges-MacBook-Pro.local>
2021-03-25 21:33:45 +0000

    Complete form submission

--- src/components/Email.js
+++ src/components/Email.js
@@ -34,7 +34,7 @@ const Email = () => {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        "form-name": contactForm,
+        "form-name": "contactForm",
         ...formState,
       }),
     })
