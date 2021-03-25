180954e929867464ee11687fb0fd139ae2257d2a
George Willens <georgewillens@Georges-MacBook-Pro.local>
2021-03-25 21:09:22 +0000

    Complete form submission

--- src/components/Email.js
+++ src/components/Email.js
@@ -13,6 +13,26 @@ const Email = () => {
     Aos.init({})
   }, [])
 
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        ...name,
+      }),
+    })
+      .then(() => navigate("/thank-you/"))
+      .catch(error => alert(error))
+  }
+
   return (
     <ContactContainer id="contact" isToggled={isToggled}>
       <LeftColumn
@@ -33,7 +53,7 @@ const Email = () => {
         <ContactForm
           name="contactForm"
           method="POST"
-          action="https://www.georgewillens.com/"
+          onSubmit={handleSubmit}
           netlify-honeypot="bot-field"
           data-netlify="true"
         >
